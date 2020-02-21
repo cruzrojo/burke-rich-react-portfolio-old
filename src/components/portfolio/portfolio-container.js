0c9d3ba932dc925fe2e7a4066c8e6f6774e898b4
cruzrojo <burkerich@gmail.com>
2020-02-21 12:00:15 -0700

    added some titles to the portfolio-container.js

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,21 +5,29 @@ import PortfolioItem from "./portfolio-item";
 export default class PortfolioContainer extends Component {
     constructor() {
         super();
-        console.log("Portfolio container has rendered");
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio...sucka!",
+            data: [
+                {title: "Quoth", url: 'quoth.com' },
+                {title: "Eventsucks", url: 'eventsucks.com' },
+                {title: "Ministry Unsafe", url: 'ministryunsafe.com' },
+                {title: "Sucky Shooter", url: 'suckyshooter.com' },
+            ]
+        }
     }
    
     portfolioItems() {
-        const data = ["Quoth", "Eventsucks", "Ministry Unsafe", "Sucky Shooter"];
 
-        return data.map(item => {
-            return <PortfolioItem title={item} />
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} />
         })
     }
 
     render() {
         return (
             <div>
-                <h2>Portfolio items go here...updated for your convenience!</h2>
+                <h2>{this.state.pageTitle}</h2>
 
                 {this.portfolioItems()}
             </div>
