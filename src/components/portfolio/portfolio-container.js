b394905bb8b657488e7451b436d612b3c704593e
cruzrojo <burkerich@gmail.com>
2020-02-25 15:47:04 -0700

    rendering live data from DevCamp Space. This is awesome...!!!

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -10,20 +10,10 @@ export default class PortfolioContainer extends Component {
     this.state = {
       pageTitle: "Welcome to my portfolio",
       isLoading: false,
-      data: [
-        { title: "Quip", category: "eCommerce", slug: "quip" },
-        { title: "Eventbrite", category: "Scheduling", slug: "eventbrite" },
-        {
-          title: "Ministry Safe",
-          category: "Enterprise",
-          slug: "ministry-safe"
-        },
-        { title: "SwingAway", category: "eCommerce", slug: "swingaway" }
-      ]
+      data: []
     };
 
     this.handleFilter = this.handleFilter.bind(this);
-    this.getPortfolioItems = this.getPortfolioItems.bind(this);
   }
 
   handleFilter(filter) {
@@ -39,6 +29,9 @@ export default class PortfolioContainer extends Component {
       .get("https://burkemrich.devcamp.space/portfolio/portfolio_items")
       .then(response => {
         console.log("response data", response);
+        this.setState({
+          data: response.data.portfolio_items
+        })
       })
       .catch(error => {
         console.log(error);
@@ -47,10 +40,15 @@ export default class PortfolioContainer extends Component {
 
   portfolioItems() {
     return this.state.data.map(item => {
+      console.log("item")
       return (
-        <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />
+        <PortfolioItem title={item.name} url={item.url} slug={item.id} />
       );
     });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
   }
 
   render() {
@@ -58,7 +56,6 @@ export default class PortfolioContainer extends Component {
       return <div>Loading...</div>;
     }
 
-    this.getPortfolioItems();
 
     return (
       <div>
