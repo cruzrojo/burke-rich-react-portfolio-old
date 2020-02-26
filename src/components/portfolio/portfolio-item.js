99c1507ebe918e4a7166ffe9811007a88e6b5152
cruzrojo <burkerich@gmail.com>
2020-02-26 09:16:23 -0700

    changes from yesterday evening

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -8,10 +8,17 @@ export default function(props) {
   // - description: description
   // - id: id
 
-  const { id, description, thumb_image_url, logo } = props.item;
+  const { id, description, thumb_image_url, logo, name } = props.item;
   return (
     <div>
-      <div>{description}</div>
+      <div>
+        <h1>{name}</h1>
+        </div>
+      <img src={thumb_image_url} alt="Thumbnail"/>
+      <img src={logo} alt="Logo"/>
+      <div>
+        <h2>{description}</h2>
+        </div>
       <Link to={`/portfolio/${id}`}>Link</Link>
     </div>
   );
