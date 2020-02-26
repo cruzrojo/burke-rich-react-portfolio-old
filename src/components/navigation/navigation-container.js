f5eec413f76a89e56bdd964751ae6795e3415d8a
cruzrojo <burkerich@gmail.com>
2020-02-26 11:57:42 -0700

    added some styles to make an underline on all the links

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -15,13 +15,23 @@ export default class NavigationContainer extends Component {
         return (
             <div className="nav-wrapper">
                 <div className="left-side">
-                    <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-                    <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
-                    <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-                    <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                    {false ? <button>Add Blog</button> : null }
+                    <div className="nav-link-wrapper">
+                        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+                    </div>
+                        
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+                    </div>
+
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>    
+                    </div>
+                    
+                    <div className="nav-link-wrapper">
+                        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                    </div>
                 </div>
-                <div>Burke Rich</div>
+                <div className="right-side">Burke M Rich</div>
             </div>
         )
     }
