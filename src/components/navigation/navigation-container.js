c94d0ab98e3e8d548ee928722971a62866d3fbeb
cruzrojo <burkerich@gmail.com>
2020-02-26 11:20:10 -0700

    added a navigation.scss and some divs to the navigation-container.js. Created a nav-wrapper, left-side and right-side classes of divs

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -13,12 +13,15 @@ export default class NavigationContainer extends Component {
 
     render() {
         return (
-            <div>
-                <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-                <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
-                <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
-                <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                {false ? <button>Add Blog</button> : null }
+            <div className="nav-wrapper">
+                <div className="left-side">
+                    <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
+                    <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
+                    <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>
+                    <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
+                    {false ? <button>Add Blog</button> : null }
+                </div>
+                <div>Burke Rich</div>
             </div>
         )
     }
