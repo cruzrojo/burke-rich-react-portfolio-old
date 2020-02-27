5856a57b5ea6c74605219f83feb0db1ce2acb551
cruzrojo <burkerich@gmail.com>
2020-02-27 16:06:10 -0700

    created the login.js and auth.js to be able to pass props between them. Altered app.js to handle the login events

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -36,17 +36,19 @@ export default class Login extends Component {
       )
       .then(response => {
         if (response.data.status === "created") {
-          console.log("You can come in...");
+          this.props.handleSuccessfulAuth();
         } else {
           this.setState({
             errorText: "Wrong email or password"
           });
+          this.props.handleUnsuccessfulAuth();
         }
       })
       .catch(error => {
         this.setState({
           errorText: "An error occurred"
         });
+        this.props.handleUnsuccessfulAuth();
       });
 
     event.preventDefault();
@@ -66,7 +68,6 @@ export default class Login extends Component {
             placeholder="Your email"
             value={this.state.email}
             onChange={this.handleChange}
-            // autoComplete="off"
           />
 
           <input
