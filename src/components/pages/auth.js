df9e6b64432bd51a28ac37b9e58b8d4e861d2cb4
cruzrojo <burkerich@gmail.com>
2020-02-27 12:00:05 -0700

    added an auth folder and a login.js and added code to auth.js to the code

--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,25 +1,22 @@
 import React, { Component } from "react";
-import loginImg from "../../../static/assets/images/auth/login.jpg"
+import Login from "../auth/login";
+import loginImg from "../../../static/assets/images/auth/login.jpg";
 
 export default class Auth extends Component {
-    constructor() {
-        super()
-    }
+  render() {
+    return (
+      <div className="auth-page-wrapper">
+        <div
+          className="left-column"
+          style={{
+            backgroundImage: `url(${loginImg})`
+          }}
+        />
 
-    render() {
-        return (
-          <div className="auth-page-wrapper">
-            <div
-              className="left-column"
-              style={{
-                backgroundImage: `url(${loginImg})`
-              }}
-            />
-    
-            <div className="right-column">
-              <h1>Login component goes here...</h1>
-            </div>
-          </div>
-        );
-      }
-    }
+        <div className="right-column">
+          <Login />
+        </div>
+      </div>
+    );
+  }
+}
