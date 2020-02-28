48fb43bb2dd818b2791fdaffca0b65fdee4f6710
cruzrojo <burkerich@gmail.com>
2020-02-28 13:58:30 -0700

    made changes to refactor teh navigation-container.js file to be a functional

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,44 +1,38 @@
-import React, { Component } from "react";
-import { NavLink } from 'react-router-dom';
-import moment from "moment";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-export default class NavigationContainer extends Component {
-    constructor() {
-        super();
+const NavigationComponent = props => {
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
+        </div>
 
-        // this.state = {
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+        </div>
 
-        // }
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+        </div>
 
-    }
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+        </div>
+      </div>
 
-    render() {
-        return (
-            <div className="nav-wrapper">
-                <div className="left-side">
-                    <div className="nav-link-wrapper">
-                        <NavLink exact to="/" activeClassName="nav-link-active">Home</NavLink>
-                    </div>
-                        
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/about-me" activeClassName="nav-link-active">About</NavLink>
-                    </div>
+      <div className="right-side">JORDAN HUDGENS</div>
+    </div>
+  );
+};
 
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/contact" activeClassName="nav-link-active">Contact</NavLink>    
-                    </div>
-                    
-                    <div className="nav-link-wrapper">
-                        <NavLink to="/blog" activeClassName="nav-link-active">Blog</NavLink>
-                    </div>
-                </div>
-                <div className="right-side">
-                    <div>Burke M Rich</div>
-                    <div>
-                        <p1>{moment().format("MMMM Do YYYY, h:mm:ss a")}</p1>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
+export default NavigationComponent;
